a746ae00156c3e3090d1fd2d0b68cb6fdab714d8
Mahmoud Adbais <mahmoud.adbais@gmail.com>
2021-03-05 20:46:31 +0300

    initial commit

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 
 # ----------------------------------------------------------------------------#
